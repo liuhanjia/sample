4959174d3a6c44771fa11bc427dea50740c32e91
liuhanjia <liu.hanjia@le-yao.com>
2018-11-07 18:10:38 +0800

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '13099740@qq.com';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
